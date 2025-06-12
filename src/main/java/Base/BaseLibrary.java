168898c2c8f1a844712f1a560cfd80cfb4917096
serkan <serkangur002@gmail.com>
2025-06-12 11:13:28 +0300

    Fix Cases

--- src/main/java/Base/BaseLibrary.java
+++ src/main/java/Base/BaseLibrary.java
@@ -1,35 +1,60 @@
 package Base;
 
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class BaseLibrary extends Data {
 
 
-    public void Get(String urlPath) {
-        RestAssured.get(urlPath).then().statusCode(200);
+    @Step("{urlPath} GET isteği gönderilir")
+    public Response Get(String urlPath) {
+        Response response = RestAssured.get(urlPath);
+        System.out.println("Response: " + response.getBody().asString());
+        return response;
     }
 
-    public void Post(String urlPath, Map<String, Object> bodyPayload, Map<String, Object> headerPayload) {
-        RestAssured.given().body(bodyPayload).headers(headerPayload).contentType(ContentType.JSON).when()
-                .post(urlPath).then().statusCode(200);
+    @Step("{urlPath} POST isteği gönderilir")
+    public Response Post(String urlPath, Map<String, Object> bodyPayload, Map<String, Object> headerPayload) {
+        Response response = RestAssured.given().body(bodyPayload).headers(headerPayload).contentType(ContentType.JSON).when()
+                .post(urlPath);
+        System.out.println("Response: " + response.getBody().asString());
+        return response;
     }
 
-    public void Put(String urlPath, Map<String, Object> bodyPayload, Map<String, Object> headerPayload) {
-        RestAssured.given().body(bodyPayload).headers(headerPayload).contentType(ContentType.JSON).when()
-                .put(urlPath).then().statusCode(200);
+    @Step("{urlPath} PUT isteği gönderilir")
+    public Response Put(String urlPath, Map<String, Object> bodyPayload, Map<String, Object> headerPayload) {
+        Response response = RestAssured.given().body(bodyPayload).headers(headerPayload).contentType(ContentType.JSON).when()
+                .put(urlPath);
+        System.out.println("Response: " + response.getBody().asString());
+        return response;
+
     }
 
 
-    public void Delete(String urlPath) {
-        RestAssured.delete(urlPath).then().statusCode(200);
+    @Step("{urlPath} DELETE isteği gönderilir")
+    public Response Delete(String urlPath) {
+        Response response = RestAssured.delete(urlPath);
+        System.out.println("Response: " + response.getBody().asString());
+        return response;
+
     }
 
-    public void Get(String urlPath, Map<String, Object> queryParamsPayLoad) {
-        RestAssured.given().queryParams(queryParamsPayLoad).when()
-                .get(urlPath).then().statusCode(200);
+    @Step("{urlPath} GET isteği gönderilir")
+    public Response Get(String urlPath, Map<String, Object> queryParamsPayLoad) {
+        Response response = RestAssured.given().queryParams(queryParamsPayLoad).when()
+                .get(urlPath);
+        System.out.println("Response: " + response.getBody().asString());
+        return response;
+    }
+
+
+    @Step("Assert kontrolü sağlanır")
+    public void AssertEquals(String expected, String actual) {
+        Assert.assertEquals(actual, expected);
     }
 }
--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -1,6 +1,8 @@
 import Base.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -71,6 +73,39 @@ public class UserTests extends BaseTest {
     @Test(description = "Get - /v2/user/logout - Logout ")
     public void Logout() {
         Get(URL + "/v2/user/logout");
+
+    }
+
+    @Test(description = "Get - User Information Control ")
+    public void UserInformationControl() {
+
+        Map<String, Object> headerPayload = new HashMap<>();
+        headerPayload.put("accept", "application/json");
+        headerPayload.put("Content-Type", "application/json");
+
+
+        Map<String, Object> bodyPayload = new HashMap<>();
+        bodyPayload.put("id", "32654895132");
+        bodyPayload.put("username", "serkangurrandom");
+        bodyPayload.put("firstName", "serkan");
+        bodyPayload.put("lastName", "gür");
+        bodyPayload.put("email", "denemeSerkan3@gmail.com");
+        bodyPayload.put("password", "Deneme-2");
+        bodyPayload.put("phone", "5555555227");
+        bodyPayload.put("userStatus", "1");
+
+        Post(URL + "/v2/user", bodyPayload, headerPayload);
+        Response response= Get(URL + "/v2/user/serkangurrandom");
+        AssertEquals(response.jsonPath().getString("id"),"32654895132");
+        AssertEquals(response.jsonPath().getString("username"),"serkangurrandom");
+        AssertEquals(response.jsonPath().getString("firstName"),"serkan");
+        AssertEquals(response.jsonPath().getString("lastName"),"gür");
+        AssertEquals(response.jsonPath().getString("email"),"denemeSerkan3@gmail.com");
+        AssertEquals(response.jsonPath().getString("password"),"Deneme-2");
+        AssertEquals(response.jsonPath().getString("phone"),"5555555227");
+        AssertEquals(response.jsonPath().getString("userStatus"),"1");
+
+
 
     }
 }
